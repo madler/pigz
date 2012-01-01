9d41d4d2c1af9ec7eb590bce9ce488a1333461ed
Mark Adler <madler@alumni.caltech.edu>
2012-01-01 09:13:44 -0800

    Update version number to 2.2.2.

--- pigz.c
+++ pigz.c
@@ -1,6 +1,6 @@
 /* pigz.c -- parallel implementation of gzip
  * Copyright (C) 2007, 2008, 2009, 2010, 2011, 2012 Mark Adler
- * Version 2.2.1  1 Jan 2012  Mark Adler
+ * Version 2.2.2  xx Jan 2012  Mark Adler
  */
 
 /*
@@ -129,9 +129,10 @@
                        Fix construction and printing of 32-bit check values
                        Add --rsyncable functionality
    2.2.1   1 Jan 2012  Fix bug in --rsyncable buffer management
+   2.2.2  xx Jan 2012  -
  */
 
-#define VERSION "pigz 2.2.1\n"
+#define VERSION "pigz 2.2.2\n"
 
 /* To-do:
     - make source portable for Windows, VMS, etc. (see gzip source code)
