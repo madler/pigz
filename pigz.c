e01aa83d0c4545593517be63441f02f816d41c40
Mark Adler <madler@alumni.caltech.edu>
2013-03-08 13:10:31 -0800

    Avoid unused variable warning for NOTHREAD.

--- pigz.c
+++ pigz.c
@@ -2652,8 +2652,6 @@ local int outb(void *desc, unsigned char *buf, unsigned len)
 #ifndef NOTHREAD
     static thread *wr, *ch;
 
-    (void)desc;
-
     if (g.procs > 1) {
         /* if first time, initialize state and launch threads */
         if (outb_write_more == NULL) {
@@ -2692,6 +2690,8 @@ local int outb(void *desc, unsigned char *buf, unsigned len)
         return 0;
     }
 #endif
+
+    (void)desc;
 
     /* if just one process or no threads, then do it without threads */
     if (len) {
