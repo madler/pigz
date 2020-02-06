5373336d4efd753e08b95ee4d897bed6760fd69f
neurolabusc <rorden@sc.edu>
2020-02-06 08:24:37 -0500

    Add support for Intel GZip

---
+++ SuperBuild/External-INTEL-ZLIB.cmake
@@ -0,0 +1,14 @@
+set(INTEL_BRANCH master) # INTEL zlib branch
+
+ExternalProject_Add(zlib
+    GIT_REPOSITORY "${git_protocol}://github.com/neurolabusc/zlib"
+    GIT_TAG "${INTEL_BRANCH}"
+    SOURCE_DIR intel-zlib
+    BINARY_DIR intel-zlib-build
+    CMAKE_ARGS
+        -Wno-dev
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+        -DCMAKE_INSTALL_PREFIX:PATH=${DEP_INSTALL_DIR}
+)
+
+set(ZLIB_ROOT ${DEP_INSTALL_DIR})
