f60c05b334c963977fee89137771e00c20ee9391
neurolabusc <rorden@sc.edu>
2020-02-19 14:51:32 -0500

    Download zlib and pthreads on demand for MSVC

--- src/win/msvc.bat
+++ src/win/msvc.bat
@@ -1,14 +1,19 @@
+REM zlib
+git clone https://github.com/rordenlab/zlib.git
 cd zlib
 nmake -f win32\Makefile.msc
-cd ..\thread
+cd ..
+REM pthreads
+git clone https://github.com/indigo-astronomy/pthreads4w
+cd pthreads4w
 nmake clean VC-static
 cd ..
 rm *.obj
 
 
-cl -c ../pigz.c /utf-8 /Idirent /Ithread /Izlib -DNOZOPFLI
-cl -c ../yarn.c /utf-8 /Idirent /Ithread /Izlib
-cl -c ../try.c /utf-8 /Idirent /Ithread /Izlib
-cl /Fepigz pigz.obj yarn.obj try.obj  /utf-8 /Idirent /Ithread /Izlib  /link  shell32.lib zlib/zlib.lib thread/libpthreadVC3.lib /NODEFAULTLIB:MSVCRT
+cl -c ../pigz.c /utf-8 /Idirent /Ipthreads4w /Izlib -DNOZOPFLI
+cl -c ../yarn.c /utf-8 /Idirent /Ipthreads4w /Izlib
+cl -c ../try.c /utf-8 /Idirent /Ipthreads4w /Izlib
+cl /Fepigz pigz.obj yarn.obj try.obj  /utf-8 /Idirent /Ipthreads4w /Izlib  /link  shell32.lib zlib/zlib.lib pthreads4w/libpthreadVC3.lib /NODEFAULTLIB:MSVCRT
+rm *.obj
 REM pigz -f -k Описание.pdf 测试.obj fx.pdf
-REM  pigz -d -f -k Описание.pdf.gz 测试.obj.gz
