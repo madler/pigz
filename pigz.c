5847cf2716df312198abc43f262357431f01b1e4
Matt Cover <werekraken@gmail.com>
2025-03-04 16:10:03 -0700

    Set FLEVEL to 0 for level 0 (no compression).
    This replicates the behavior of zlib's deflate().

--- pigz.c
+++ pigz.c
@@ -1154,7 +1154,7 @@ local length_t put_header(void) {
         unsigned head;
         head = (0x78 << 8) +        // deflate, 32K window
                (g.level >= 9 ? 3 << 6 :
-                g.level == 1 ? 0 << 6:
+                g.level == 0 || g.level == 1 ? 0 << 6:
                 g.level >= 6 || g.level == Z_DEFAULT_COMPRESSION ? 1 << 6 :
                 2 << 6);            // optional compression level clue
         head += 31 - (head % 31);   // make it a multiple of 31
